08b7c8a0a2ecc3261993292f2ddb5a759cd90dcc
moalam7 <mohammedalamx@gmail.com>
2024-04-24 09:43:01 -0400

    tip-js

---
+++ JS_Basics/demos/exercise3.js
@@ -0,0 +1,6 @@
+var firstName = "Mohammed";
+var lastName = "Alam";
+var cityName = "Queens";
+var stateName = "New York";
+
+console.log("Hi, my name is " + firstName + " " + lastName + " and I live in " + cityName + ", " + stateName + ".");
--- JS_Basics/demos/hello-world.js
+++ JS_Basics/demos/hello-world.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+var age = 12;
+var name;
+
+name = "mohammed";
+
+console.log(name);
