94daefc4a8e24c0e7378a345d7d38e1267ae4b12
moalam7 <mohammedalamx@gmail.com>
2024-04-27 23:15:12 -0400

    major_if_statements

---
+++ exercises/major.js
@@ -0,0 +1,14 @@
+var studentName = "John Doe";
+var studentMajor = "MKT";
+
+var majorName;
+
+if (studentMajor == "BIOL") {
+  majorName = "Biology";
+} else if (studentMajor == "CSCI") {
+  majorName = "Computer Science";
+} else if (studentMajor == "MKT") {
+  majorName = "Marketing";
+}
+
+console.log(studentName + " is majoring in " + majorName);
